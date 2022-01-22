0306c285ba290cb204a0e2c74781fb640554c4d7
Deyi233 <98209978+Deyi233@users.noreply.github.com>
2022-01-22 20:30:40 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '叔叔很生气';
+    if (score <= 99) return '你所热爱的就是你的生活';
+    if (score <= 149) return '您就是蒙古上单？';
+    if (score <= 199) return '您马什么时候4啊？';
     return '人？';
 }
 
